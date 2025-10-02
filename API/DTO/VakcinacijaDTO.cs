20b21212eac61e417de65595022416764b41cf2c
Natasa <natasaelez21@gmail.com>
2025-10-02 12:20:34 +0200

    Prvi commit

---
+++ API/DTO/VakcinacijaDTO.cs
@@ -0,0 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+
+public class VakcinacijaDto
+{
+    public int Id { get; set; }
+    public required string NazivVakcine { get; set; }
+    public DateOnly DatumPrimanja { get; set; }
+    public int Doza { get; set; }
+    public string? Napomena { get; set; }
+    public required string PacijentIme { get; set; }
+    public required string PacijentPrezime { get; set; }
+}
